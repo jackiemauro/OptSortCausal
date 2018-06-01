83bf748855e8af3a0f72df8342fa685c7d5377f7
jackiemauro <jacqueline.amauro@gmail.com>
2018-06-01 13:16:16 -0700

    edited combinedopt code so that you can choose the algorithms you want for the nuisance parameters. one option is ranger2 which lets ranger output probabilities for each a but can't account for the different distances. this may be ok if we don't think your chances of being assigned to prison a depend on how far it is from your home (assumption we made in the shift paper)

--- CombinedOutput.R
+++ CombinedOutput.R
@@ -1,3 +1,28 @@
+#### old, not great dataset. all results before 5/29/2018 10am are on this####
+nm = names(dat)
+covs = cbind(dat[,which(nm=="loslastloc"):which(nm=='ageyrs')],#dat[,which(nm=='total_time')],
+             dat[,which(nm=='visitslastloc1'):which(nm=='highschoolgrad')],
+             dat[,which(nm=='numofvisitsever')],
+             dat[,which(nm=='child')], dat[,which(nm=='parent')], dat[,which(nm=='spouse')],
+             dat[,which(nm=='friendsnonfamily')],
+             dat[,which(nm=='numofpriormisconducts')]
+)
+
+df <- as.data.frame(cbind(dat$NCRecid3, dat$A, covs))
+to.keep <- complete.cases(df)
+df <- df[complete.cases(df),]  # highschool grad the most missing, 63 unobserved values
+names(df) <- c('y', 'A',sapply(c(1:dim(covs)[2]), function(k) paste('x',k,sep = "")))
+obsD <- dat$total_time[to.keep]
+dist.mat <- dist.mat[to.keep,]
+
+##### using combined opt and sl for both mu and pi ####
+out.combo <- constr.opt.causal(df, aLevel = dist.mat, obsD = obsD)
+write.csv(out.combo$ifvals, "~jacquelinemauro/Dropbox/sorter/SLifvalsUnconstr.csv")
+write.csv(out.combo$assig.vec, "~jacquelinemauro/Dropbox/sorter/SLassigvecUnconstr.csv")
+write.csv(out.combo$phihat, "~jacquelinemauro/Dropbox/sorter/SLphihatUnconstr.csv")
+write.csv(out.combo$muhat, "~jacquelinemauro/Dropbox/sorter/SLmuhatUnconstr.csv")
+write.csv(out.combo$pihat, "~jacquelinemauro/Dropbox/sorter/SLpihatUnconstr.csv")
+
 #### June 1: using better dataset ####
 # leaving out visits at last location
 dat <- read.csv('~jacquelinemauro/MergedData.csv')[,-1]
@@ -43,65 +68,17 @@ sum.stats.tab <- data.frame(Mean = sum.stats.means, SD = sum.stats.sds)
 rownames(sum.stats.tab) <- nms[-2]
 print(xtable(sum.stats.tab, caption = 'Summary Statistics'), file = "~jacquelinemauro/Dropbox/sorter/summarystats.tex")
 
-#### old, not great dataset. all results before 5/29/2018 10am are on this####
-nm = names(dat)
-covs = cbind(dat[,which(nm=="loslastloc"):which(nm=='ageyrs')],#dat[,which(nm=='total_time')],
-             dat[,which(nm=='visitslastloc1'):which(nm=='highschoolgrad')],
-             dat[,which(nm=='numofvisitsever')],
-             dat[,which(nm=='child')], dat[,which(nm=='parent')], dat[,which(nm=='spouse')],
-             dat[,which(nm=='friendsnonfamily')],
-             dat[,which(nm=='numofpriormisconducts')]
-)
+##### output all nuisance parameters and unconstrained estimate using ranger ####
+# bad bad bad
+out.combo.rg <- constr.opt.causal(df, aLevel = dist.mat, obsD = obsD,mu.algo = 'ranger', pi.algo = 'ranger2')
+write.csv(out.combo.rg$ifvals, "~jacquelinemauro/Dropbox/sorter/RGifvalsUnconstrNewdat.csv")
+write.csv(out.combo.rg$assig.vec, "~jacquelinemauro/Dropbox/sorter/RGassigvecUnconstrNewdat.csv")
+write.csv(out.combo.rg$phihat, "~jacquelinemauro/Dropbox/sorter/RGphihatUnconstrNewdat.csv")
+write.csv(out.combo.rg$muhat, "~jacquelinemauro/Dropbox/sorter/RGmuhatUnconstrNewdat.csv")
 
-df <- as.data.frame(cbind(dat$NCRecid3, dat$A, covs))
-to.keep <- complete.cases(df)
-df <- df[complete.cases(df),]  # highschool grad the most missing, 63 unobserved values
-names(df) <- c('y', 'A',sapply(c(1:dim(covs)[2]), function(k) paste('x',k,sep = "")))
-obsD <- dat$total_time[to.keep]
-dist.mat <- dist.mat[to.keep,]
-
-##### using combined opt and sl for both mu and pi ####
-out.combo <- constr.opt.causal(df, aLevel = dist.mat, obsD = obsD)
-write.csv(out.combo$ifvals, "~jacquelinemauro/Dropbox/sorter/SLifvalsUnconstr.csv")
-write.csv(out.combo$assig.vec, "~jacquelinemauro/Dropbox/sorter/SLassigvecUnconstr.csv")
-write.csv(out.combo$phihat, "~jacquelinemauro/Dropbox/sorter/SLphihatUnconstr.csv")
-write.csv(out.combo$muhat, "~jacquelinemauro/Dropbox/sorter/SLmuhatUnconstr.csv")
-write.csv(out.combo$pihat, "~jacquelinemauro/Dropbox/sorter/SLpihatUnconstr.csv")
-
-##### using combined opt and sl for both mu and pi on better dataset ####
-dat <- read.csv('~jacquelinemauro/MergedData.csv')[,-1]
-dat$no.visits.last.loc <- (1 - dat$visitslastlocyn1)
-dat$no.childvisits <- (1 - dat$childyn)
-dat$maxtime <- apply(dat[,2:26],1,max)
-dat <- dat[-which(dat$NCRecid.Event == 'reincarceration'),] #drop if they go back for parole violation
-
-options(na.action='na.pass')
-county.f = factor(dat$CountyClass); county.dummies = model.matrix(~county.f)[,-c(1,2)]
-minName.f = factor(dat$minTimeName); minName.dummies = model.matrix(~minName.f)[,-c(1,2)]
-pris.dummies <- dat[,which(names(dat)=='alb'):which(names(dat)=='pit')]
-dist.mat <- dat[,which(nm=='ALB_time'):which(nm=='WAM_time')]
-names(dist.mat)<-names(pris.dummies)
-dat$A <- apply(pris.dummies,1,which.max)
-dat$nmA <- apply(pris.dummies,1,function(x) names(pris.dummies)[which.max(x)])
-
-nm = names(dat)
-covs = cbind(dat[,which(nm=="loslastloc"):which(nm=='white')],dat[,which(nm=='urban'):which(nm=='ageyrs')],
-             dat[,which(nm=='custody_level'):which(nm=='numofpriorinc')],
-             dat[,which(nm=='visitslastloc1'):which(nm=='highschoolgrad')],
-             dat[,which(nm=='numofvisitsever')],
-             dat[,which(nm=='child')], dat[,which(nm=='parent')], dat[,which(nm=='spouse')],
-             dat[,which(nm=='friendsnonfamily')],
-             dat[,which(nm=='numofpriormisconducts')]
-)
-
-df <- as.data.frame(cbind(dat$NCRecid3, dat$A, covs))
-to.keep <- complete.cases(df)
-df <- df[complete.cases(df),]  # highschool grad the most missing, 63 unobserved values
-names(df) <- c('y', 'A',sapply(c(1:dim(covs)[2]), function(k) paste('x',k,sep = "")))
-obsD <- dat$total_time[to.keep]
-dist.mat <- dist.mat[to.keep,]
 
 ##### output all nuisance parameters and unconstrained estimate ####
+# not run
 out.combo2 <- constr.opt.causal(df, aLevel = dist.mat, obsD = obsD)
 write.csv(out.combo2$ifvals, "~jacquelinemauro/Dropbox/sorter/SLifvalsUnconstrNewdat.csv")
 write.csv(out.combo2$assig.vec, "~jacquelinemauro/Dropbox/sorter/SLassigvecUnconstrNewdat.csv")
--- R/CombinedOpt.R
+++ R/CombinedOpt.R
@@ -14,12 +14,14 @@
 #' influence function values that can be used in the constrained optimization
 #' procedure.
 
-constr.opt.causal <- function(df,aLevel,obsD){
+constr.opt.causal <- function(df,aLevel,obsD,
+                              mu.algo = 'superlearner',
+                              pi.algo = 'superlearner',
+                              sl.lib = c("SL.gam","SL.glm","SL.glmnet","SL.glm.interaction", "SL.mean","SL.ranger","SL.rpart")){
   library('ranger')
   library('SuperLearner')
   library('randomForest')
   library('plyr')
-  sl.lib = c("SL.gam","SL.glm","SL.glm.interaction", "SL.mean","SL.ranger")
   n = dim(df)[1]
   df$A <- as.numeric(df$A)
   Avals <- unique(df$A); n.avals <- length(Avals)
@@ -37,18 +39,34 @@ constr.opt.causal <- function(df,aLevel,obsD){
     aMat.train <- aLevel[s==rnd,]
     aMat.test <- aLevel[s!=rnd,]
 
-    ### train E(Y|A,X)
-    #mu <- ranger::ranger(y~., data = train.df, write.forest = TRUE)
-    mu <- SuperLearner(Y = train.df$y, X = Xtrain, family = binomial(), SL.library = sl.lib)
+    ### train E(Y|A,X) & predict E(Y|A = a, X) for each a
+    if(mu.algo == 'ranger'){
+      list.out = lapply(Avals, function(a) mu.pred.rg(train.df = train.df, Xtrain = Xtrain, Xtest = Xtest, a.val = a,aMat.train=aMat.train, aMat.test=aMat.test))
+      preds <- matrix(unlist(list.out),ncol = length(Avals), byrow = F)
+      }
+    if(mu.algo == 'superlearner'){
+      list.out = lapply(Avals, function(a) mu.pred.sl(train.df = train.df, Xtrain = Xtrain, Xtest = Xtest, a.val = a,aMat.train=aMat.train, aMat.test=aMat.test))
+      preds <- matrix(unlist(list.out),ncol = length(Avals), byrow = F)
+    }
 
-    ### predict E(Y|A = a, X) for each a
-    preds <- matrix(unlist(lapply(Avals, function(a) mat.pred(df = test.df, A = a, mu = mu, aLevel = aMat.test))),
-                    ncol = length(Avals), byrow = F)
 
-    ### get P(A=a|X) for each a
-    phat.pre = matrix(unlist(lapply(Avals, function(a) sl.pred(a.val=a,train.df=train.df,Xtrain=Xtrain,Xtest=Xtest,
-                                                               aMat.train=aMat.train, aMat.test=aMat.test)))
-                      ,ncol = length(Avals), byrow = F)
+    ### train P(A|X) & predict P(A=a|X) for each a
+    if(pi.algo == 'logistic'){
+      list.out = lapply(Avals, function(a) pi.pred.lg(a.val=a,train.df=train.df,Xtrain=Xtrain,Xtest=Xtest,aMat.train=aMat.train, aMat.test=aMat.test))
+      phat.pre = matrix(unlist(list.out),ncol = length(Avals), byrow = F)
+    }
+    if(pi.algo == 'ranger'){
+      list.out = lapply(Avals, function(a) pi.pred.rg(a.val=a,train.df=train.df,Xtrain=Xtrain,Xtest=Xtest,aMat.train=aMat.train, aMat.test=aMat.test))
+      phat.pre = matrix(unlist(list.out),ncol = length(Avals), byrow = F)
+    }
+    if(pi.algo == 'superlearner'){
+      list.out = lapply(Avals, function(a) pi.pred.sl(a.val=a,train.df=train.df,Xtrain=Xtrain,Xtest=Xtest,aMat.train=aMat.train, aMat.test=aMat.test))
+      phat.pre = matrix(unlist(list.out),ncol = length(Avals), byrow = F)
+    }
+    if(pi.algo == 'ranger2'){
+      phat.pre = pi.pred.rg2(train.df=train.df,Xtrain=Xtrain,Xtest=Xtest)
+    }
+
 
     # truncate pi values
     phat.pre[phat.pre < 1e-3] = 1e-3
@@ -79,44 +97,52 @@ constr.opt.causal <- function(df,aLevel,obsD){
   psi = mean(psihat)
   sd = mean(sdhat)
 
-  #return(list(psi = psi,sd = sd, ifvals = ifs, assig.vec = assig.vec, phihat = phihat))
-
-  # what if we do more similarly to the unconstrained version and minimize just based
-  # on mu, then calculate the if afterwards?
   return(list(psi = psi,sd = sd, ifvals = ifs, assig.vec = assig.vec, phihat = phihat,
               muhat = out.mu, pihat = out.pi))
 }
 
 
-
-mat.pred <- function(df, A, mu, aLevel){
-  t <- df
-  t$obsD <- aLevel[,A]
-  t$A <- A
-  preds = c(predict.SuperLearner(mu, newdata = t, onlySL = TRUE)[[1]])
+# nuisance parameter estimation functions
+mu.pred.sl <- function(a.val, train.df, Xtrain, Xtest, sl.lib= c("SL.gam","SL.glm","SL.glm.interaction", "SL.mean","SL.ranger"), aMat.train, aMat.test){
+  Xtrain$obsD <- aMat.train[,a.val]
+  Xtest$obsD <- aMat.test[,a.val]
+  out = SuperLearner(Y = train.df$y, X = Xtrain, family = binomial(), SL.library = sl.lib)
+  preds = c(predict.SuperLearner(out, newdata = Xtest, onlySL = TRUE)[[1]])
   return(preds)
-  #predict(mu, t)$pre
 }
-sl.pred <- function(a.val, train.df, Xtrain, Xtest, sl.lib= c("SL.gam","SL.glm","SL.glm.interaction", "SL.mean","SL.ranger"), aMat.train, aMat.test){
-  # need to add obsD
+mu.pred.rg <- function(a.val, train.df, Xtrain, Xtest, sl.lib= c("SL.gam","SL.glm","SL.glm.interaction", "SL.mean","SL.ranger"), aMat.train, aMat.test){
+  d <- cbind(train.df$y,Xtrain); names(d)<-c('y',names(Xtrain))
+  d$obsD <- aMat.train[,a.val]
+  Xtest$obsD <- aMat.test[,a.val]
+  preds <- predict(ranger::ranger(y~., data = d, write.forest = T), Xtest)$pre
+  return(preds)
+}
+pi.pred.sl <- function(a.val, train.df, Xtrain, Xtest, sl.lib= c("SL.gam","SL.glm","SL.glm.interaction", "SL.mean","SL.ranger"), aMat.train, aMat.test){
   Xtrain$obsD <- aMat.train[,a.val]
   Xtest$obsD <- aMat.test[,a.val]
   out = SuperLearner(Y = as.numeric(train.df$A==a.val), X = Xtrain, family = binomial(), SL.library = sl.lib)
   preds = c(predict.SuperLearner(out, newdata = Xtest, onlySL = TRUE)[[1]])
   return(preds)
 }
-lg.pred <- function(a.val, train.df, Xtrain, Xtest, aMat.train, aMat.test){
+pi.pred.lg <- function(a.val, train.df, Xtrain, Xtest, aMat.train, aMat.test){
   d <- cbind(as.numeric(a.val==train.df$A),Xtrain); names(d)<-c('A',names(Xtrain))
   d$obsD <- aMat.train[,a.val]
   Xtest$obsD <- aMat.test[,a.val]
   preds <- predict(glm(A~., data = d, family = binomial), newdata = Xtest, type = 'response')
   return(preds)
 }
-rg.pred <- function(a.val, train.df, Xtrain, Xtest, aMat.train, aMat.test){
+pi.pred.rg <- function(a.val, train.df, Xtrain, Xtest, aMat.train, aMat.test){
   library('ranger')
   d <- cbind(as.numeric(a.val==train.df$A),Xtrain); names(d)<-c('A',names(Xtrain))
   d$obsD <- aMat.train[,a.val]
   Xtest$obsD <- aMat.test[,a.val]
-  preds <- predict(ranger::ranger(as.factor(A)~., data = d, write.forest = T, probability = T), Xtest)$pre
+  preds <- predict(ranger::ranger(as.factor(A)~., data = d, write.forest = T,probability = T), Xtest)$pre
   return(preds[,2])
+}
+pi.pred.rg2 <- function(train.df, Xtrain, Xtest){
+  library('ranger')
+  # does not use the distance value
+  d <- cbind(train.df$A,Xtrain); names(d)<-c('A',names(Xtrain))
+  preds <- predict(ranger::ranger(as.factor(A)~., data = d, write.forest = T,probability = T), Xtest)$pre
+  return(preds)
 }
